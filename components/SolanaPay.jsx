051b5155e666be8468089d795ad3f12f3e2e89dc
Marc Espin Sanz <mespinsanz@gmail.com>
2022-02-11 00:15:06 +0100

    Fixes

--- components/SolanaPay.jsx
+++ components/SolanaPay.jsx
@@ -1,11 +1,12 @@
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 export default function SolanaPayButton({ children, uri }) {
     return (
         <a href={uri}>
             <button className={styles.solanaPayButton}>
                 {children}
-                <img width={65} src="/solana-pay.svg"></img>
+                <Image  width={85} height={0} src="/solana-pay.svg"></Image>
             </button>
         </a>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { useEffect } from 'react'
 import SolanaPayButton from '../components/SolanaPay'
 import styles from '../styles/Home.module.css'
